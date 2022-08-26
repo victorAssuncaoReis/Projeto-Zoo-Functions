cdefdb3b1034f14397addb543535e15e97210619
Victor <victorreis222@gmail.com>
2022-08-26 16:09:27 -0300

    Lint requisito 01

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
+const data = require('../data/zoo_data');
+const { residents } = species;
+console.log(residents);
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
 }
 
+//Implemente a função getAnimalsOlderThan que deve receber uma espécie e uma idade como parâmetro, e então retornar se todos os animais dessa espécie possuem essa idade ou são mais velhos.
+/* Verifique se todos os animais da espécie passada como parâmetro possuem a idade mínima:
+
+Os animais devem ter essa idade ou serem mais velhos.
+Retorne um valor booleano.
+
+O que será testado:
+
+A função, ao receber uma espécie e uma idade como parâmetros, deve testar se todos os animais desta espécie possuem a idade mínima especificada. */
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,16 +1,16 @@
 const { species } = require('../data/zoo_data');
+
 const data = require('../data/zoo_data');
-const [{id}] = species;
+
 /* console.log(id); */
 function getSpeciesByIds(...ids) {
-  const animal = ids.map((especie) => species.filter((id) => id.id === especie));
-  return animal.map(animal => animal[0]);
+  const animalFunc = ids.map((especie) => species.filter((id) => id.id === especie));
+  return animalFunc.map((animal) => animal[0]);
 }
 
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
-
 
 /* Faça com que a função getSpeciesByIds possa receber vários parâmetros;
 
@@ -29,5 +29,3 @@ A função getSpeciesByIds, caso não receba nenhum parâmetro, deve retornar um
 A função getSpeciesByIds, caso receba como parâmetro um único ID, deve retornar um array com a espécie referente a esse ID;
 
 A função getSpeciesByIds, caso receba mais de um ID, deve retornar um array com as espécies referentes aos IDs. */
-
-

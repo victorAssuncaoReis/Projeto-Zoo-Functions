6e1862ce7e2e762ef9c97ef564aa5f1bd439387d
Victor <victorreis222@gmail.com>
2022-08-26 16:57:08 -0300

    Requisito 03

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const { employees } = require('../data/zoo_data');
 
+function getEmployeeByName(employeeName) {
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees
+    .filter((ele) => employeeName === ele.firstName || employeeName === ele.lastName)[0];
+}
+console.log(getEmployeeByName('Nigel'));
 module.exports = getEmployeeByName;
+
+/* Implemente a função getEmployeeByName que deve buscar por pessoas colaboradoras através de seu primeiro ou último nome.
+
+Retorne um objeto vazio caso a função não receba parâmetros;
+
+Retorne as informações da pessoa colaboradora caso o parâmetro seja igual ao nome ou igual ao último nome no seguinte formato: */

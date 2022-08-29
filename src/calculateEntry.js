96e2e9ff2c2496a1a3ec69ee4e73b076b94372bc
Victor <victorreis222@gmail.com>
2022-08-29 16:01:04 -0300

    Exercício 05

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,73 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const entrants = [
+  { name:  'Lara Carvalho', age:  5 },
+  { name:  'Frederico Moreira', age:  5 },
+  { name:  'Pedro Henrique Carvalho', age:  5 },
+  { name:  'Maria Costa', age:  18 },
+  { name:  'Núbia Souza', age:  18 },
+  { name:  'Carlos Nogueira', age:  50 },
+  ];
+
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.filter((person) => person.age < 18).length,
+    adult: entrants.filter((person) => person.age >= 18 && person.age < 50).length,
+    senior: entrants.filter((person) => person.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || entrants.length === undefined) {
+    return 0;
+  }
+  return countEntrants(entrants).child * prices.child
+  + countEntrants(entrants).adult * prices.adult
+  + countEntrants(entrants).senior * prices.senior;
 }
+console.log(calculateEntry());
+console.log(calculateEntry(entrants));
 
 module.exports = { calculateEntry, countEntrants };
+
+/* A
+
+A função calculateEntry será responsável por somar o valor da entrada das pessoas no zoológico:
+Ela recebe um array e deve retornar a soma total dos valores do ingresso. Para isso:
+
+Retorne 0 se nenhum parâmetro for passado ou seja um array vazio;
+
+Utilize a função countEntrants para ter a quantidade total de pessoas por faixa etária;
+
+Realize a soma dos valores dos ingressos por faixa etária. Seu retorno deve ser parecido com esse: 187.94.
+
+De olho na dica eyes: O valor a ser cobrado pela faixa de idades também consta no arquivo de dados.
+
+Exemplo de uso da função calculateEntry:
+
+calculateEntry(entrants);
+Saída:
+
+187.94
+O que será testado:
+
+A função countEntrants:
+
+Ao receber um array de visitantes, retorna um objeto com a contagem.
+A função calculateEntry:
+
+Retorna 0 se nenhum argumento for passado;
+
+Retorna 0 se um objeto vazio for passado;
+
+Ao receber um array de pessoas com 3 crianças, 2 pessoas adultas e 1 pessoa mais velha retorna o valor correto;
+
+Ao receber um array com 1 pessoa adulta retorna o valor correto;
+
+Ao receber um array com 1 pessoa mais velha retorna o valor correto;
+
+Ao receber um array com 1 criança retorna o valor correto;
+
+Ao receber um array com 1 criança e 1 pessoa mais velha retorna o valor correto.
+ */
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,50 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+const speciesName = () => (species.map((element) => element.name));
+
+function countAnimals(animal = {}) {
+  if (animal.specie && animal.sex) {
+    return species
+      .filter((element) => element.name === animal.specie)[0].residents
+      .filter((resident) => resident.sex === animal.sex).length;
+  } if (animal.specie) {
+    return species
+      .filter((element) => element.name === animal.specie)[0].residents.length;
+  }
+  return speciesName().reduce((acc, curr) => ({ ...acc,
+    [curr]: species
+      .filter((element) => element.name === curr)[0].residents.length }), {});
 }
 
+console.log(countAnimals());
+console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'penguins', sex: 'female' }));
 module.exports = countAnimals;
+
+//* Durante o desenvolvimento desta resposta tive ajuda de Gabriel Boubee e Mirella Dourado, principalmente quanto ao uso do reduce.
+
+/* Implemente a função countAnimals que deverá contabilizar a quantidade de espécies de animais residentes no zoológico.
+
+A função countAnimals é responsável por contar a quantidade de animais que residem no zoológico.
+
+Retorne a quantidade de animais residentes por espécie caso não seja passado nenhum parâmetro. O retorno deverá ser um objeto cujo o nome de cada espécie é a chave e o total de animais (residentes) dessa espécie é o valor. Por exemplo:
+
+  {
+    lions: 4,
+    // [...]
+  }
+Retorne a quantidade de animais residentes no zoológico da espécie passada por parâmetro. Por exemplo:
+
+ao receber o argumento { specie: 'penguins' }, retorna apenas a quantidade (número) de pinguins que residem no zoológico;
+
+ao passar o argumento { specie: 'giraffes', sex: 'female' }, retorna apenas a quantidade (número) de girafas fêmeas que residem no zoológico.
+
+O que será testado:
+
+A função countAnimals, caso não receba nenhum parâmetro, deve retornar todas as espécies e a quantidade de residentes de cada uma;
+
+A função countAnimals, caso receba como parâmetro um objeto com a chave specie, deve retornar a quantidade de animais daquela espécie;
+
+A função countAnimals, caso receba como parâmetro um objeto com as chaves specie e sex, deve retornar a quantidade de animais daquela espécie, no sexo selecionado. */

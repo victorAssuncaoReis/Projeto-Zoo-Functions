afc4d319ea775aa3d50dacdd7eb9b72db2aaafe8
Victor <victorreis222@gmail.com>
2022-08-29 16:51:59 -0300

    Arrumando requisito 10 para retornar array

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -9,7 +9,7 @@ function getOldestFromFirstSpecies(id) {
   const oldestAnimal = species
     .find((element) => getSpeciesId.find((element2) => element.id === element2)).residents
     .reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
-  return oldestAnimal;
+  return Object.values(oldestAnimal);
 }
 
 console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
